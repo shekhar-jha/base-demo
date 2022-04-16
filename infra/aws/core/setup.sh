c342736d66806a91966c87feb8aab31179a6117d
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2022-04-15 22:24:07 -0500

    Fixes & Changes
    1. Added -r region as option
    
    2. Fixes based on shellcheck validation
    
    3. Added explicit return codes
    
    4. Streamline code by using PGPGetKeyIfNotExists

--- infra/aws/core/setup.sh
+++ infra/aws/core/setup.sh
@@ -5,8 +5,8 @@
 . ../../scripts/infra.sh
 . ../../scripts/tf.sh
 
-usage() { echo "Usage: $0 -e <environment name> -c <aws profile> [-d]" 1>&2; exit 1;}
-while getopts ":e:c:d" options;
+usage() { echo "Usage: $0 -e <environment name> -c <aws profile> -r <aws region> [-d]" 1>&2; exit 1;}
+while getopts ":e:c:r:d" options;
 do
   case "${options}" in
     e)
@@ -23,6 +23,14 @@ do
         usage
       fi
       ;;
+    r)
+      CLOUD_REGION="${OPTARG}"
+      if [[ "${CLOUD_REGION}" == "" ]];
+      then
+        usage
+      fi
+      ;;
+
     d)
       TF_DOWNLOAD_PLUGIN=true
       ;;
@@ -47,9 +55,13 @@ fi
 
 export ENV_NAME="${ENV_NAME:-NEW}"
 export TF_DOWNLOAD_PLUGIN=${TF_DOWNLOAD_PLUGIN:-false}
-export env_name=$(echo "${ENV_NAME}" | tr '[:upper:]' '[:lower:]')
+export CLOUD_REGION="${CLOUD_REGION:-us-east-1}"
+env_name=$(echo "${ENV_NAME}" | tr '[:upper:]' '[:lower:]')
+export env_name
 
-AWSInit "${ENV_NAME}" 'us-east-1' profile "${CLOUD_PROFILE}"
+IsAvailable Command tar "tar"
+
+AWSInit "${ENV_NAME}" "${CLOUD_REGION}" profile "${CLOUD_PROFILE}"
 if [[ $? -ne 0 ]];
 then
   echo "Failed to setup AWS using profile ${CLOUD_PROFILE}"
@@ -60,31 +72,31 @@ PGPKeyExistsInStore "${env_name}" 'KEY' 'AWS'
 key_exists_in_store=$?
 PGPKeyExistsInStore "${env_name}" 'PUB' 'AWS'
 pub_exists_in_store=$?
-if [ $key_exists_in_store -eq 1 -o $pub_exists_in_store -eq 1 ];
+if [[ $key_exists_in_store -eq 1 ]] || [[ $pub_exists_in_store -eq 1 ]];
 then
   echo "PGP keys don't exist in store"
   PGPKeyFileExists "${env_name}" 'KEY'; key_file_exists=$?
   PGPKeyFileExists "${env_name}" 'PUB'; pub_file_exists=$?
-  if [ $key_file_exists -ne 0 -a $pub_file_exists -ne 0 ];
+  if [[ $key_file_exists -ne 0 ]] && [[ $pub_file_exists -ne 0 ]];
   then
     echo "PGP key files are missing." 
     PGPCreateKeys "${env_name}"
   fi
   PGPKeyFileExists "${env_name}" 'KEY'; key_file_exists=$?
   PGPKeyFileExists "${env_name}" 'PUB'; pub_file_exists=$?
-  if [ $key_file_exists -eq 0 -a $pub_file_exists -eq 0 ];
+  if [[ $key_file_exists -eq 0 ]] && [[ $pub_file_exists -eq 0 ]];
   then
-    PGPStoreKeys ${env_name} KEY AWS
+    PGPStoreKeys "${env_name}" KEY AWS
     key_store_result=$?
     if [ $key_store_result -eq 0 ];
     then
-      PGPDeleteKeyFile ${env_name} KEY
+      PGPDeleteKeyFile "${env_name}" KEY
     fi
-    PGPStoreKeys ${env_name} PUB AWS
+    PGPStoreKeys "${env_name}" PUB AWS
     pub_store_result=$?
     if [ $pub_store_result -eq 0 ];
     then
-      PGPDeleteKeyFile ${env_name} pub
+      PGPDeleteKeyFile "${env_name}" pub
     fi
   fi
 fi
@@ -94,46 +106,42 @@ tf_home_ret_code=$?
 if [ $tf_home_ret_code -ne 0 ];
 then
   echo "Failed to locate Terraform home"
-  exit
+  exit 3
 fi
 if [ ! -d "${tf_home}" ];
 then
   echo "No Terraform home exists at ${tf_home}"
-  exit
-fi
-PGPKeyFileExists "${env_name}" 'PUB' "${tf_home}"; pub_file_exists=$?
-if [ $pub_file_exists -eq 1 ];
-then
-  PGPGetKey "${env_name}" 'PUB' 'AWS' "${tf_home}"
+  exit 4
 fi
 
+PGPGetKeyIfNotExists "${env_name}" 'PUB' 'AWS' "${tf_home}" 'Y'
 InfraApply "${env_name}" 'Terraform'; infra_apply=$?
-if [ $infra_apply -eq 0 ];
+if [ $infra_apply -ne 0 ];
 then
-  encrypted_b64_access_key=$(TFGetConfig "${env_name}" "aws_iam_user_access_key_secret_encrypt")
-  get_config_ret_code=$?
-  if [[ $get_config_ret_code -ne 0 ]];
-  then
-    echo "Failed to retrieve access key secret aws_iam_user_access_key_secret_encryp1t due to error ${get_config_ret_code}"
-    echo "${encrypted_b64_access_key}"
-    exit
-  fi
-  PGPKeyFileExists "${env_name}" 'KEY' "${tf_home}"; key_file_exists=$? 
-  if [ $key_file_exists -eq 1 ];
-  then
-    PGPGetKey "${env_name}" 'KEY' 'AWS' "${tf_home}"
-  fi
-  decryptedKey=$(echo "${encrypted_b64_access_key}"|base64 -d | PGPDecrypt "${env_name}" '' "${tf_home}" )
-  decryptedKey_ret_code=$?
-  if [[ $decryptedKey_ret_code -ne 0 ]];
-  then
-    echo "Failed to decrypt key due to error ${decryptedKey_ret_code}"
-    echo "${decryptedKey}"
-    exit
-  fi
-  echo "Decrypt: ${decryptedKey}"
-  TFCleanup "${env_name}"
-  PGPDeleteKeyFile "${env_name}" KEY "${tf_home}" 
-  PGPDeleteKeyFile "${env_name}" PUB "${tf_home}" 
+  echo "Failed to apply infrastructure changes due to error ${infra_apply}"
+  exit 5
 fi
+
+encrypted_b64_access_key=$(TFGetConfig "${env_name}" "aws_iam_user_access_key_secret_encrypt"); get_config_ret_code=$?
+if [[ $get_config_ret_code -ne 0 ]];
+then
+  echo "Failed to retrieve access key secret aws_iam_user_access_key_secret_encrypt due to error ${get_config_ret_code}"
+  echo "${encrypted_b64_access_key}"
+  exit 6
+fi
+PGPGetKeyIfNotExists "${env_name}" 'KEY' 'AWS' "${tf_home}" 'Y'
+decryptedKey=$(echo "${encrypted_b64_access_key}"|base64 -d | PGPDecrypt "${env_name}" '' "${tf_home}" )
+decryptedKey_ret_code=$?
+if [[ $decryptedKey_ret_code -ne 0 ]];
+then
+  echo "Failed to decrypt key due to error ${decryptedKey_ret_code}"
+  echo "${decryptedKey}"
+  exit 7
+fi
+echo "Decrypt: ${decryptedKey}"
+TFCleanup "${env_name}"
+PGPDeleteKeyFile "${env_name}" KEY "${tf_home}"
+PGPDeleteKeyFile "${env_name}" PUB "${tf_home}"
+
+
 
