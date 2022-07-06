2d929664da7b2f94a77f5d5f776053f3709b5262
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2022-07-06 14:52:56 -0500

    Added support for single code across 1. Lambda Invoke 2. API Gateway 3. Functional URL

--- go-lambda/cmd/handler.go
+++ go-lambda/cmd/handler.go
@@ -9,6 +9,7 @@ import (
 	errext "github.com/shekhar-jha/base-demo/go-utils/pkg/err"
 	"github.com/shekhar-jha/base-demo/go-utils/pkg/log"
 	"os"
+	"strings"
 )
 
 func init() {
@@ -73,22 +74,89 @@ func (handler lambdaHandler) Invoke(ctx context.Context, payload []byte) ([]byte
 	return handler(ctx, payload)
 }
 
+type ProtocolType int
+
+const (
+	Unknown ProtocolType = iota
+	TCP
+	HTTP
+	AWS_LAMBDA_HTTP
+	AWS_LAMBDA_INVOKE
+)
+
+var ErrRequestParsingError = errext.NewErrorTemplate("Parsing Error", "Failed to parse {{ .Data.Parameter }} payload as JSON due to error {{ .Data.ParseError }}")
+var ErrResponseGenerationError = errext.NewErrorTemplate("Response Generation Error", "Failed to generate response payload due to error {{ .Data }}")
+
 func simpleLambdaHandler(handler Handle, ctx context.Context, payload []byte) ([]byte, error) {
-	var response = events.APIGatewayV2HTTPResponse{
-		StatusCode:        200,
-		Headers:           nil,
-		MultiValueHeaders: nil,
-		Body:              "",
-		IsBase64Encoded:   false,
-		Cookies:           nil,
+	readData := map[string]interface{}{}
+	unmarshalErr := json.Unmarshal(payload, &readData)
+	if unmarshalErr != nil {
+		return nil, ErrRequestParsingError.New(struct {
+			Parameter  string
+			ParseError error
+		}{Parameter: "Request", ParseError: unmarshalErr})
 	}
-	var requestContext = &simpleContext{request: string(payload)}
+	requestObject := map[string]interface{}{}
+	logger.LogWithLevel(log.Debug, "Parsed request object %#v", readData)
+	var eventType ProtocolType
+	if _, isHTTP := asMap(readData, "requestContext", "http"); isHTTP {
+		logger.LogWithLevel(log.Debug, "Request is an Lambda HTTP Request")
+		eventType = AWS_LAMBDA_HTTP
+		requestId, _ := asString(readData, "requestContext", "requestId")
+		logger.LogWithLevel(log.Debug, "Request Id %s", requestId)
+		if queryParams, hasQueryParams := asMap(readData, "queryStringParameters"); hasQueryParams {
+			copyMap(queryParams, requestObject)
+			logger.LogWithLevel(log.Debug, "Added query parameters %#v", queryParams)
+		}
+		if body, hasBody := asString(readData, "body"); hasBody {
+			logger.LogWithLevel(log.Debug, "Request body is present")
+			if contentType, hasContentType := asString(readData, "headers", "content-type"); hasContentType {
+				switch strings.ToLower(contentType) {
+				case "application/json":
+					logger.LogWithLevel(log.Debug, "Parsing content type application/json")
+					bodyUnmarshalError := json.Unmarshal([]byte(body), &requestObject)
+					if bodyUnmarshalError != nil {
+						return nil, ErrRequestParsingError.New(struct {
+							Parameter  string
+							ParseError error
+						}{Parameter: "Body", ParseError: bodyUnmarshalError})
+					}
+				default:
+					logger.LogWithLevel(log.Warn, "Failed to identify request body content type %s for request %s", contentType, requestId)
+				}
+			} else {
+				logger.LogWithLevel(log.Warn, "Request body is present but no content-type header is available for validation.")
+			}
+		}
+	} else {
+		eventType = AWS_LAMBDA_INVOKE
+		requestObject = readData
+	}
+	var requestContext = &simpleContext{request: requestObject}
 	logger.LogWithLevel(log.Debug, "Invoking handler with context %v, request %#v", ctx, requestContext.request)
 	err := handler(requestContext)
 	logger.LogWithLevel(log.Debug, "Response %#v; Error %v", requestContext.response, err)
-	if err != nil {
-		response.Body = fmt.Sprintf("{ \"Message\" : \"%s\"}", err.Error())
-	} else {
+	switch eventType {
+	case AWS_LAMBDA_INVOKE:
+		if err != nil {
+			return nil, err
+		}
+		if requestContext.response != nil {
+			output, marshalErr := json.Marshal(requestContext.response)
+			return output, marshalErr
+		} else {
+			return []byte("{}"), nil
+		}
+	case AWS_LAMBDA_HTTP:
+		var response = events.APIGatewayV2HTTPResponse{
+			StatusCode:        200,
+			Headers:           nil,
+			MultiValueHeaders: nil,
+			Body:              "",
+			IsBase64Encoded:   false,
+			Cookies:           nil,
+		}
+
 		marshaledValue, marshalErr := json.Marshal(requestContext.response)
 		if marshalErr != nil {
 			response.Body = fmt.Sprintf("{ \"Message\" : \"%#v\"}", marshalErr)
@@ -96,7 +164,66 @@ func simpleLambdaHandler(handler Handle, ctx context.Context, payload []byte) ([
 			response.StatusCode = 200
 			response.Body = string(marshaledValue)
 		}
+		respByte, httpMarshalErr := json.Marshal(response)
+		return respByte, httpMarshalErr
+	default:
+		unsupportedFormatErr := fmt.Sprintf("Event type %s is not supported", eventType)
+		return nil, ErrResponseGenerationError.New(unsupportedFormatErr)
 	}
-	respByte, _ := json.Marshal(response)
-	return respByte, nil
+}
+
+func asString(input map[string]interface{}, key ...string) (string, bool) {
+	totalKeys := len(key)
+	if input == nil || totalKeys == 0 {
+		return "", false
+	}
+	applicableMap := input
+	if totalKeys > 1 {
+		if processedMap, validProcess := asMap(applicableMap, key[0:totalKeys-1]...); validProcess {
+			applicableMap = processedMap
+		} else {
+			return "", false
+		}
+	}
+	if value, hasValue := applicableMap[key[totalKeys-1]]; hasValue && value != nil {
+		if valueAsString, isString := value.(string); isString {
+			return valueAsString, true
+		}
+	}
+	return "", false
+}
+func asMap(input map[string]interface{}, key ...string) (map[string]interface{}, bool) {
+	asMapLogger := logger.Push("asMap", key)
+	logger.LogWithLevel(log.Trace, "Input: %#v", input)
+	returnValue := map[string]interface{}{}
+	if input == nil {
+		return returnValue, false
+	}
+	totalKeys := len(key)
+	asMapLogger.LogWithLevel(log.Trace, "Total keys: %d", totalKeys)
+	if totalKeys > 0 {
+		var applicableMap = input
+		for remainingKeys := totalKeys; remainingKeys >= 1; remainingKeys-- {
+			asMapLogger.LogWithLevel(log.Trace, "Applicable map: %v", applicableMap)
+			asMapLogger.LogWithLevel(log.Trace, "Remaining keys: %d", remainingKeys)
+			if value, hasValue := applicableMap[key[totalKeys-remainingKeys]]; hasValue && value != nil {
+				if valueAsMap, isMap := value.(map[string]interface{}); isMap {
+					applicableMap = valueAsMap
+				} else {
+					return returnValue, false
+				}
+			} else {
+				return returnValue, false
+			}
+		}
+		return applicableMap, true
+	} else {
+		return input, true
+	}
+}
+
+func copyMap(src map[string]interface{}, dest map[string]interface{}) {
+	for queryKey, value := range src {
+		dest[queryKey] = value
+	}
 }
